6fe436c44792eb497e11a2bf85b93414238b537d
Trenton1Fisher <trenton0fisher@gmail.com>
2024-01-26 17:33:24 -0800

    added db and drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,18 @@
+import { sql } from 'drizzle-orm'
+import { text, integer, sqliteTable } from 'drizzle-orm/sqlite-core'
+
+const Leaderboard = sqliteTable('leaderboard', {
+  id: integer('id').notNull().primaryKey(),
+  username: text('username').notNull(),
+  wpm: integer('wpm').notNull(),
+  date: text('date').default(sql`CURRENT_DATE`),
+})
+
+const User = sqliteTable('user', {
+  userId: text('uid').notNull().primaryKey(),
+  testsStarted: integer('tests_started'),
+  testsCompleted: integer('tests_completed'),
+  topWPM: integer('top_wpm'),
+  avgWPM: integer('avg_wpm'),
+  avgAccuracy: integer('avg_accuracy'),
+})
