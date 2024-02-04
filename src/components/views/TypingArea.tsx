1cbe076f45f5e246c3f04ed5e8e5d00e7be71fcd
Trenton1Fisher <trenton0fisher@gmail.com>
2024-02-03 21:31:00 -0800

    idk

--- src/components/views/TypingArea.tsx
+++ src/components/views/TypingArea.tsx
@@ -1,7 +1,9 @@
+import testSentences from "@/constants/tests"
+
 export default function TypingArea() {
   return (
     <div>
-      <p>l</p>
+      <p>{testSentences.lowerCaseOnly}</p>
     </div>
   )
 }
